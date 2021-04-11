84d41fd50423b7e92afb00553b6527abfae8aa5c
devins2518 <drsingh2518@icloud.com>
2021-04-11 11:29:55 -0500

    fix: remove muts and run formatting (#1)
    * chore: formatting
    
    * fix: remove muts and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 fn main() {
-	for pat in 0..4 {
-		let mut block = "█";
+    for pat in 0..4 {
+        let block = if pat == 0 { "▄" } else { "█" };
 
-		if pat == 0 {
-			block = "▄";
-		}
+        for color in 0..8 {
+            let panepart = if pat == 3 {
+                "\u{001b}[1m ▀▀▀▀".to_string()
+            } else {
+                ["████\u{001b}[1m", block].join("")
+            };
 
-		for color in 0..8 {
-			let mut panepart = ["████\u{001b}[1m", block].join("");
+            let line = &[
+                "\u{001b}[3",
+                &color.to_string(),
+                "m",
+                &panepart,
+                "  \u{001b}[0m",
+            ]
+            .join("");
+            print!("{}", line);
+        }
 
-			if pat == 3 {
-				panepart = "\u{001b}[1m ▀▀▀▀".to_string();
-			}
-
-			let line = &["\u{001b}[3", &color.to_string(), "m", &panepart, "  \u{001b}[0m"].join("");
-			print!("{}", line);
-		}
-
-		print!("\n")
-	}
+        print!("\n")
+    }
 }

fe2e4b8c3a19a214d4035d1f4a421793a3cd726d
Devin Singh <drsingh2518@icloud.com>
2021-04-11 11:00:35 -0500

    chore: formatting

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 fn main() {
-	for pat in 0..4 {
-		let mut block = "█";
+    for pat in 0..4 {
+        let mut block = "█";
 
-		if pat == 0 {
-			block = "▄";
-		}
+        if pat == 0 {
+            block = "▄";
+        }
 
-		for color in 0..8 {
-			let mut panepart = ["████\u{001b}[1m", block].join("");
+        for color in 0..8 {
+            let mut panepart = ["████\u{001b}[1m", block].join("");
 
-			if pat == 3 {
-				panepart = "\u{001b}[1m ▀▀▀▀".to_string();
-			}
+            if pat == 3 {
+                panepart = "\u{001b}[1m ▀▀▀▀".to_string();
+            }
 
-			let line = &["\u{001b}[3", &color.to_string(), "m", &panepart, "  \u{001b}[0m"].join("");
-			print!("{}", line);
-		}
+            let line = &[
+                "\u{001b}[3",
+                &color.to_string(),
+                "m",
+                &panepart,
+                "  \u{001b}[0m",
+            ]
+            .join("");
+            print!("{}", line);
+        }
 
-		print!("\n")
-	}
+        print!("\n")
+    }
 }
